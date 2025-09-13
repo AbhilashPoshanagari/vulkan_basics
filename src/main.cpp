204d431a66cdaf1f5e093ecbcc3676d26c263161
PAbhilashTRC <PAbhilash@trcsolutions.com>
2025-09-13 17:07:31 +0530

    added window to for vulkan

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,20 @@
 #include <cstdint>
 #include <cmath>
 #include <gsl/gsl>
+#include <glfw_initialization.h>
+#include <GLFW/glfw3.h>
 
-int main(std::int32_t argc, gsl::zstring* argv) {
-	gsl::span<gsl::zstring> arguments(argv, argc);
+int main() {
+  std::cout << "hello world from vulkan" << std::endl;
+  veng::GlfwInitialization _glfw;
 
+  gsl::not_null<GLFWwindow*> window = glfwCreateWindow(800, 600, "Abhi vulkan engen", nullptr, nullptr);
+  gsl::final_action _clean_window([window]() -> void {
+    glfwDestroyWindow(window);
+  });
 
-	std::cout << "Hello world from Abhi" << std::endl;
-	return 0;
+  while (!glfwWindowShouldClose(window)) {
+    glfwPollEvents();
+  }
+	return EXIT_SUCCESS;
 }
